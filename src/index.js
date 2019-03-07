2fab2a290c06c625dd8b6a13e4cfe4d7d993fde7
ameyallibg <bgameyalli@gmail.com>
2019-03-07 09:15:18 -0600

    subiendo login sin firebase

--- src/index.js
+++ src/index.js
@@ -1,93 +1,150 @@
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+const signIn = document.getElementById("sign-in-new");
+const password = document.getElementById("password-new");
+const buttonSignIn = document.getElementById("button-sign-in-new");
 
-ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
-  });
-  ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      // List of OAuth providers supported.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
-  });
-
-  var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-      },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
-      }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
-  };
-
-  // The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+const signInRegister = document.getElementById("sign-in-reg");
+const passwordRegister = document.getElementById("password-reg");
+const buttonSignInRegister = document.getElementById("button-sign-in-reg");
+const show = document.getElementById("show")
+const modalWarning = document.getElementById("modal-warning")
+const modalInvalidEmail =document.getElementById("modal-invalid-email")
+const nameUser = document.getElementById("name")
+const userNameWall = document.getElementById("userNameWall")
 
 
-var provider = new firebase.auth.GoogleAuthProvider();
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
+
+
+    buttonSignIn.addEventListener("click",() => {
+        let signInValue = signIn.value;
+        let passwordValue = password.value;
+        console.log(signInValue);
+        console.log(passwordValue);
+
+   firebase.auth().createUserWithEmailAndPassword(signInValue, passwordValue)
+   .then(function(){
+       verification()
+   })
+   .catch(function(error) {
     // Handle Errors here.
-    var errorCode = error.code;
+    // var errorCode = error.code;
     var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
+    // console.log(errorCode);
+    console.log(errorMessage);
+    modalInvalidEmail.innerHTML = ` <div class="alert alert-warning" role="alert">
+    <p> ${errorMessage} </p>
+  </div>`;
   });
 
+    })
 
-  firebase.auth().signInWithRedirect(provider);
 
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // ...
+    buttonSignInRegister.addEventListener("click", () =>{
+
+        let signInValue = signInRegister.value;
+        let passwordValue = passwordRegister.value;
+        firebase.auth().signInWithEmailAndPassword(signInValue, passwordValue).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            show.innerHTML= ` <div class="alert alert-warning" role="alert">
+            <p>${errorCode} o ${errorMessage}</p>
+          </div>`;
+            // ...
+          });
+
+
+
+    })
+const state = () => {
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+      showUser(user)
+      console.log("existe usuario activo");
+    //
+
+    // User is signed in.
+    var displayName = user.displayName;
+    console.log(displayName)
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    console.log(user.emailVerified);
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+
+
+
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+
+    show.innerHTML= ` <div class="alert alert-warning" role="alert">
+    <p>Sesion no iniciada</p>
+  </div>`;
+
+  }
+});
+}
+
+state();
+
+const showUser = (user) => {
+    var user = user;
+    if (user.emailVerified) {
+        //window.location.replace = "wall.html"
+        show.innerHTML =  `<button onclick="signOutUserRegister()">Cerrar sesion</button> `;
+        // localStorage.setItem('botonCerrar',buttonClose)
+        //     window.location.href = "wall.html"
+
+        //     userNameWall.innerHTML = "Hola " + localStorage.getItem('botonCerrar')
+
+    }else{
+        console.log("no verificado");
+
+
     }
-    // The signed-in user info.
-    var user = result.user;
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+
+
+}
+
+const signOutUserRegister = () => {
+        firebase.auth().signOut()
+        .then(function(){
+            //window.location.replace("wall.html")
+            console.log("saliendo...");
+
+
+        })
+        .catch(function(error){
+            console.log(error);
+
+
+        })
+
+
+}
+
+const verification = () => {
+    var user = firebase.auth().currentUser;
+
+user.sendEmailVerification().then(function() {
+  // Email sent.
+  modalWarning.innerHTML= ` <div class="alert alert-warning" role="alert">
+  <p>Se te ha enviado un correo de verificacion de Usuario</p>
+</div>`;
+
+}).catch(function(error) {
+    console.log("error");
+
+  // An error happened.
+});
+}
+// const getName = () => {
+// let nameValue = nameUser.value;
+// localStorage.setItem('nombreusuario',nameValue);
+// window.location.href = "wall.html";
+// }
